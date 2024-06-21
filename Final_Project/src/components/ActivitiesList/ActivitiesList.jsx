edd516c5b7a234e66fb7d2e517438b5aa2dadbb4
liatReuven <liatreuven08@gmail.com>
2024-06-22 00:42:56 +0300

    now it works

--- Final_Project/src/components/ActivitiesList/ActivitiesList.jsx
+++ Final_Project/src/components/ActivitiesList/ActivitiesList.jsx
@@ -5,59 +5,58 @@ import { nanoid } from "nanoid";
 import EfooterS from "../../Elements/EfooterS";
 
 export default function ActivitiesList() {
-  const location = useLocation();
-  const date = location.state;
-  const inputRef = useRef(null);
-  const [activities, setActivities] = useState([
-    "text",
-    "text2",
-    "text3",
-    "text4",
-    "text5",
-  ]);
+    const location = useLocation();
+    const date = location.state;
+    const inputRef = useRef(null);
+    const [activities, setActivities] = useState([]);
 
-  function addActivity() {
-    setActivities((prev) => [...prev, inputRef.current.value]);
-    inputRef.current.value = "";
-  }
+    function addActivity(value) {
+        if (!value) return;
+        inputRef.current.value = "";
+        setActivities((prev) => [...prev, value]);
+    }
 
-  return (
-    <div className="page-container usual-background flex-column space-between page-height-with-footer">
-      <div className="flex-row gap-20">
-        <span className="rounded-teal-container width-100 font-20">
-          {fullHourFormat(date.getHours(), date.getMinutes())}
-        </span>
-        <button
-          disabled
-          type="text"
-          className="flex-length cool-input padding-h-10px"
-        />
-      </div>
-      <div className="two-column-grid gap-20 scroll">
-        {activities.map((activity) => (
-          <button
-            key={nanoid()}
-            className="rounded-teal-container flex-column center activity-background"
-            //   onClick={() => handleDayClick(day)}
-          >
-            <h2 className="font-30">{activity}</h2>
-          </button>
-        ))}
-      </div>
-      <div className="flex-column gap-8">
-        <input
-          ref={inputRef}
-          className="font-30 padding-h-10px cool-input height-input"
-          placeholder="הקלד פעילות חדשה..."
-        />
-        <button
-          onClick={addActivity}
-          className="rounded-teal-container font-30"
-        >
-          הוספת פעילות
-        </button>
-      </div>
-      {EfooterS}
-    </div>
-  );
+    return (
+        <div className="page-container usual-background flex-column space-between page-height-with-footer">
+            <div className="flex-row gap-20">
+                <span className="rounded-teal-container width-100 font-20">
+                    {fullHourFormat(date.getHours(), date.getMinutes())}
+                </span>
+                <button
+                    disabled
+                    type="text"
+                    className="flex-length cool-input padding-h-10px"
+                />
+            </div>
+            {activities.length > 0 ? (
+                <div className="two-column-grid gap-20 scroll height-60">
+                    {activities.map((activity) => (
+                        <button
+                            key={nanoid()}
+                            className="rounded-teal-container flex-column center activity-background height-80"
+                            //   onClick={() => handleDayClick(day)}
+                        >
+                            <h2 className="font-30">{activity}</h2>
+                        </button>
+                    ))}
+                </div>
+            ) : (
+                <h1 className="white">אין פעילויות</h1>
+            )}
+            <div className="flex-column gap-8">
+                <input
+                    ref={inputRef}
+                    className="font-30 padding-h-10px cool-input height-input"
+                    placeholder="הקלד פעילות חדשה..."
+                />
+                <button
+                    onClick={() => addActivity(inputRef.current.value)}
+                    className="rounded-teal-container font-30"
+                >
+                    הוספת פעילות
+                </button>
+            </div>
+            {EfooterS}
+        </div>
+    );
 }
