b83e0ed2c3a70e477dace30d2504b8ed78f92e20
liatReuven <liatreuven08@gmail.com>
2024-06-24 22:09:25 +0300

    CalendarStaff

--- Final_Project/src/components/CalendarStaff/CalendarStaff.jsx
+++ Final_Project/src/components/CalendarStaff/CalendarStaff.jsx
@@ -1,16 +1,46 @@
 import React, { useState } from "react";
+import { formatDate } from "../../utils/functions";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
+import { nanoid } from "nanoid";
+import EfooterS from "../../Elements/EfooterS";
 
 export default function CalendarStaff() {
-    const [date, setDate] = useState(new Date());
+  const [date, setDate] = useState(new Date());
+  const [activities, setActivities] = useState([
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "f",
+    "f",
+    "f",
+  ]);
 
-    return (
-        <div className="page-container week-calendar-container flex-column">
-            <Calendar value={date} onChange={setDate} calendarType="hebrew" />
-            <div>
-                <h2>עדכון לוז</h2>
-            </div>
+  return (
+    <div className="page-container page-height-with-footer flex-column gap-20">
+      <div className="week-calendar-container height-50-percent padding-20 rounded-corners-25">
+        <Calendar value={date} onChange={setDate} calendarType="hebrew" />
+      </div>
+      <div className="week-calendar-container  height-50-percent padding-h-10px rounded-corners-25">
+        <h1 className="rounded-teal-container">
+          לוז והתראות - {formatDate(date)}
+        </h1>
+        <div className="flex-column width-full height-70-percent gap-8 scroll">
+          {activities.map((activity) => (
+            <button
+              key={nanoid()}
+              className="rounded-teal-container activity-background "
+              //   onClick={() => handleDayClick(day)}
+            >
+              <h2 className="font-30">{activity}</h2>
+            </button>
+          ))}
         </div>
-    );
+      </div>
+      {EfooterS}
+    </div>
+  );
 }
